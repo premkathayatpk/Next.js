e08bd0d91261d39f85cc0010ce42598c46a99ffd
premkathayatpk <premkathayat13@gmail.com>
2025-10-28 21:49:03 +0545

    nav

--- e-commerce/src/components/Header.jsx
+++ e-commerce/src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import style from '@styles/header.module.css'
 import Container from './Container'
+import  Link  from 'next/link';
 
 const Header = () => {
   return (
     <header>
      <Container/>
-<h1>Header</h1>
+<h1>NepSopping</h1>
+
+<ul>
+  <li>
+  
+    <Link href='/'>Home</Link>
+  </li>
+  <li>
+    <Link href='/store'>Store</Link>
+  </li>
+</ul>
       
     </header>
   )
